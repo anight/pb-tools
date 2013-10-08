60f8a2fbc1001517c9e2bdc6962855e5df9c509f
Marko Kevac <marko@kevac.org>
2013-10-08 13:25:22 +0400

    converted to python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pb_tools',
+        version='0.1',
+        description='Python libraries for GPBS protokol',
+        url='',
+        author='Andrei Nigmatulin',
+        author_email='anight@corp.badoo.com',
+        license='MIT',
+        packages=['pb_tools'],
+        zip_safe=False)
