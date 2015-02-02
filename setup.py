fd0c6ce9649f99bfaa4310e69a398e9c769106e7
Marko Kevac <m.kevac@corp.badoo.com>
2015-02-02 11:52:41 +0300

    [master] update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pb_tools',
-        version='0.1',
+        version='0.2',
         description='Python support code for Google Protocol Buffers',
         url='',
         author='Andrei Nigmatulin',
